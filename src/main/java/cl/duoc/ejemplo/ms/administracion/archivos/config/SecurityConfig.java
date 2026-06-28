76c27c55753a238d1853a614d65e8bfe5c3ec6d8
DaenNo1 <da.ericesn@duocuc.cl>
2026-06-28 19:59:59 -0400

    Create SecurityConfig.java

---
+++ src/main/java/cl/duoc/ejemplo/ms/administracion/archivos/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package cl.duoc.azuread.ejemplo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+		http.cors(Customizer.withDefaults()).authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+				.oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+		return http.build();
+	}
+}
